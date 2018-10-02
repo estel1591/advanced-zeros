30be6c625a93187f0aaa8128317ab50f9aa24c60
estel1591 <estel1591@mail.ru>
2018-10-02 23:20:15 +0300

    update index.js (correct function getZerosCount)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let currentBase = base;
+    let currentNum = number;
+
+
+    let primeFactor = [];
+    let factorNumber = 2;
+    let bestFactor = 0;
+    let pow = 1;
+
+    getNum(currentBase, factorNumber, primeFactor);
+    bestFactor = getMaxOfArray(primeFactor);
+
+    if (primeFactor.reverse().indexOf(bestFactor) != primeFactor.reverse().lastIndexOf(bestFactor)) {
+        pow = primeFactor.reverse().lastIndexOf(bestFactor) + 1;
+    }
+
+
+    let result = 0;
+    for ( var i = bestFactor; currentNum/i > 1; i *= bestFactor ) {
+        result += Math.trunc(currentNum/i);
+    }
+
+
+    return Math.trunc(result/pow);
+};
+
+function getNum (num, factorNumber, arr) {
+    if (num % factorNumber === 0) {
+        arr.push(factorNumber);
+        num = num / factorNumber;
+        getNum(num, factorNumber, arr);
+    } else if (num % factorNumber !== 0) {
+        if (!(num <= factorNumber)) {
+            factorNumber++;
+            getNum(num, factorNumber, arr);
+        }
+    }
+}
+
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
 }
